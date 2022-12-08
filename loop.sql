249bc6a2d9d38e2d6a813e93f6e2eb433a120b71
piustonen <72132818+piustonen@users.noreply.github.com>
2022-12-08 15:18:18 +0200

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -1,15 +1,25 @@
-DO $$
- DECLARE
-     genres_id   genres.genre_id%TYPE;
-     genres_name genres.genre_name%TYPE;
+SELECT * FROM cloud_releases;
+CREATE TABLE relcopy AS SELECT * FROM cloud_releases;
+SELECT * FROM relcopy;
+DELETE FROM relcopy;
 
+ DO $$
+ DECLARE 
+ 	rel_id relcopy.release_id%TYPE;
+ 	gen_id relcopy.genre_id%TYPE;
+ 	s_id relcopy.song_id%TYPE;
+	
  BEGIN
-     genres_id := 'ID';
-     genres_name := 'GenreName';
-     FOR counter IN 1..20
-         LOOP
-            INSERT INTO Genres (genre_id, genre_name)
-             VALUES (genres_id || counter, genres_name || counter);
-         END LOOP;
+ 	rel_id   := '2000';
+ 	gen_id   := '100';
+ 	s_id     := '0';
+ 	FOR counter IN 1..9
+ 		LOOP
+ 			INSERT INTO relcopy(release_id, genre_id, song_id, release_date)
+ 			VALUES (rel_id || counter, 
+					gen_id || counter, 
+					s_id || counter, 
+					(SELECT CURRENT_DATE));
+ 		END LOOP;
  END;
  $$
